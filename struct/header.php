23a0cb633020ad57c0967136e8085c779df35c90
svetlana_i <ionova@rospartner.ru>
2018-02-11 22:50:44 +0300

    Menu and header

--- struct/header.php
+++ struct/header.php
@@ -1,13 +1,3 @@
-<!-- <!DOCTYPE html>
-<html>
-<head>
-    <title>Minecraft Server</title>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css?v=<?=md5(date('his'))?>">
-</head> -->
-
-<!-- -----------------------С моего сайта---------------------------- -->
 <!DOCTYPE html>
 <html>
 <head>
